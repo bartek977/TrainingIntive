abd450ac387c8386da16fb21a28c8e6778075013
bartek977 <bartekturkosz@gmail.com>
2021-07-15 16:26:25 +0200

    refactor

--- app/src/main/java/com/example/trainingintive/ui/ActivitiesFragment.kt
+++ app/src/main/java/com/example/trainingintive/ui/ActivitiesFragment.kt
@@ -14,10 +14,9 @@ class ActivitiesFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_activities, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_activities, container, false)
 
 }
