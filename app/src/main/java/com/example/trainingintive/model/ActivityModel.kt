8548445fef70b3d2bc6240e9b7bdbefe8d552247
bartek977 <bartekturkosz@gmail.com>
2021-07-15 11:53:00 +0200

    delete unnecessary comment

--- app/src/main/java/com/example/trainingintive/model/ActivityModel.kt
+++ app/src/main/java/com/example/trainingintive/model/ActivityModel.kt
@@ -2,6 +2,6 @@ package com.example.trainingintive.model
 
 data class ActivityModel(
     val activity: String,
-    val type: String, //someday enum
+    val type: String,
     val participants: Int
 )
