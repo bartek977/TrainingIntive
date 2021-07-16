dd4194e89a7252b43734d0d5359d6fed79072515
bartek977 <bartekturkosz@gmail.com>
2021-07-16 08:37:39 +0200

    refactor

--- app/src/main/java/com/example/trainingintive/SplashActivity.kt
+++ app/src/main/java/com/example/trainingintive/SplashActivity.kt
@@ -7,9 +7,9 @@ import android.os.Handler
 import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
