2b236d556db8706baf8bd313c229ee1ef0430a13
bartek977 <bartekturkosz@gmail.com>
2021-07-16 13:49:35 +0200

    refactor

--- app/src/main/java/com/example/trainingintive/SplashActivity.kt
+++ app/src/main/java/com/example/trainingintive/SplashActivity.kt
@@ -3,8 +3,6 @@ package com.example.trainingintive
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
 
