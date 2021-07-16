cbb1e58b09697f4ad7ad8992d01ca82e98005184
Bartłomiej Turkosz <49306295+bartek977@users.noreply.github.com>
2021-07-16 08:34:53 +0200

    add fragment navigation (#2)
    add fragment navigation

---
+++ app/src/main/java/com/example/trainingintive/ui/ActivitiesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.trainingintive.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.trainingintive.R
+
+class ActivitiesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_activities, container, false)
+
+}
