d378289c322075382c0ac7c6b70a66a90c0a4c9f
bartek977 <bartekturkosz@gmail.com>
2021-07-19 08:52:36 +0200

    download image url

--- app/src/main/java/com/example/trainingintive/network/DogImageApiService.kt
+++ app/src/main/java/com/example/trainingintive/network/DogImageApiService.kt
@@ -2,8 +2,11 @@ package com.example.trainingintive.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
@@ -16,12 +19,13 @@ private val moshi = Moshi.Builder()
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
 interface DogImageApiService {
     @GET("api/breeds/image/random")
-    fun getDogImage(): Response<DogImage>
+    fun getDogImage(): Single<DogImage>
 }
 
 object DogImageApi {
