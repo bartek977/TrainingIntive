dd86e27a8b9bbbb6236cfefc6db55aeee9567c82
bartek977 <bartekturkosz@gmail.com>
2021-08-18 14:04:39 +0200

    [REFACTOR-REVIEW]add exception class extension for getting error id message

--- app/src/main/java/com/example/trainingintive/dog_images_feature/presentation/DogImageViewModel.kt
+++ app/src/main/java/com/example/trainingintive/dog_images_feature/presentation/DogImageViewModel.kt
@@ -7,9 +7,9 @@ import com.example.trainingintive.dog_images_feature.data.repository.DogImageRep
 import com.example.trainingintive.dog_images_feature.domain.model.DogImageUrl
 import com.example.trainingintive.navigators.MainNavigator
 import com.example.trainingintive.rx.SchedulersProvider
-import com.example.trainingintive.util.ErrorMessageId
 import com.example.trainingintive.util.MainScreenEvent
 import com.example.trainingintive.util.plusAssign
+import com.example.trainingintive.util.toErrorTextId
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import java.lang.Exception
 import javax.inject.Inject
@@ -36,9 +36,8 @@ class DogImageViewModel @Inject constructor(
                         insertDogImageIntoLocalDatabase(dog)
                     },
                     {
-                        val errorMessageId = ErrorMessageId.getId(it as Exception)
                         mainNavigator.sendEvent(
-                            MainScreenEvent.Error(errorMessageId)
+                            MainScreenEvent.Error(it.toErrorTextId())
                         )
                     }
                 )
