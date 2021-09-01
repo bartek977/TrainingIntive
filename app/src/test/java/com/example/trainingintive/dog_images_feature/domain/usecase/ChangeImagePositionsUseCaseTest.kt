fe95002cdc996d06f90adc6e805396463e4be639
bartek977 <bartekturkosz@gmail.com>
2021-09-01 16:21:45 +0200

    add ChangeImagePositionsUseCase test

---
+++ app/src/test/java/com/example/trainingintive/dog_images_feature/domain/usecase/ChangeImagePositionsUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.example.trainingintive.dog_images_feature.domain.usecase
+
+import com.example.trainingintive.dog_images_feature.domain.model.DogImageUrl
+import com.example.trainingintive.dog_images_feature.domain.repository.DogImageRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.rxjava3.core.Completable
+import org.junit.Test
+
+class ChangeImagePositionsUseCaseTest {
+
+    val repository: DogImageRepository = mockk()
+    val tested = ChangeImagePositionsUseCase(repository)
+
+    @Test
+    fun execute() {
+        val list: List<DogImageUrl> = mockk()
+        every { repository.changeImagePosition(any(), any(), any()) } returns Completable.complete()
+
+        tested.execute(list, 0, 1)
+
+        verify { repository.changeImagePosition(list, 0, 1) }
+    }
+
+}
